d0c3b65cb796cf3ef17ead65bfb251d4229be457
LabSR-UT <142353737+LabSR-UT@users.noreply.github.com>
2025-03-06 16:53:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from skimage.io import imread
 
 st.title('Propiedades de una imagen digital')
 
-uploaded_file = st.file_uploader("Seleccione una imagen...", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Seleccione una imagen...", type=["jpg", "jpeg", "png", "tif"])
 col1, col2 = st.columns(2)
 
 if uploaded_file is not None:
@@ -21,5 +21,3 @@ if uploaded_file is not None:
         st.write(f"Cantidad de pixeles: {image.size}")
         st.write(f"Tipo de dato: {image.dtype}")
         st.write(f"Capas por pixel: {image.ndim}")
-else:
-    st.write("Favor usar imagenes en formato jpg/png")
