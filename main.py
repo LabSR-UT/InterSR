082ab2f2b3b8731109262c603f221979ef678c68
LabSR-UT <142353737+LabSR-UT@users.noreply.github.com>
2025-03-06 11:09:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import streamlit as st
 import numpy as np
-
 from skimage.io import imread
 
 st.title('Propiedades de una imagen digital')
 
 uploaded_file = st.file_uploader("Seleccione una imagen...", type=["jpg", "jpeg", "png"])
+col1, col2 = st.columns(2)
 
 if uploaded_file is not None:
-    #image = cv2.imread(uploaded_file)
-    image = imread(uploaded_file)    
- 
-    st.image(image, caption='Imagen', width=400)
-    st.write(f"Tipo de imagen: {type(image)}")
-    st.write(f"Dimensiones de la imagen: {image.shape}")
-    st.write(f"Cantidad de pixeles: {image.size}")
-    st.write(f"Tipo de dato: {image.dtype}")
-    st.write(f"Capas por pixel: {image.ndim}")
+    image = imread(uploaded_file)
 
-st.write("Nota: usar imagenes en formato jpg o png")
+    with col1:
+        st.header("Imagen")
+        st.image(image, caption='Imagen', width=200)
+
+    with col2:
+        st.header("Propiedades")            
+        st.write(f"Tipo de imagen: {type(image)}")
+        st.write(f"Dimensiones de la imagen: {image.shape}")
+        st.write(f"Cantidad de pixeles: {image.size}")
+        st.write(f"Tipo de dato: {image.dtype}")
+        st.write(f"Capas por pixel: {image.ndim}")
+else:
+    st.write("Favor usar imagenes en formato jpg/png")
