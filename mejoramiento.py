251b5d60babd04298b273fdaf29fda38d8c9d6b4
LabSR-UT <142353737+LabSR-UT@users.noreply.github.com>
2025-03-16 21:32:12 -0500

    Add files via upload

---
+++ mejoramiento.py
@@ -0,0 +1,72 @@
+import streamlit as st
+
+import skimage as ski
+from skimage import exposure, io
+from skimage import transform
+import numpy as np
+from skimage.color import rgb2gray
+import cv2
+
+st.title('Mejoramiento')
+
+uploaded_file = st.file_uploader("Seleccione una imagen...", type=["jpg", "jpeg", "png"])
+img = io.imread(uploaded_file)
+
+option = st.selectbox(
+    "Tipo de mejoramiento",
+    ("Contraste", "Ecualizacion", "Paso Alto", "Paso Bajo", "Color"),
+)
+
+if option=="Contraste":
+	brightness = st.number_input("Ingrese valor de brillo: (0, +Inf)", value=10.0, placeholder="Ingrese un numero...")
+	# Adjusts the contrast by scaling the pixel values by 2.3 
+	contrast = st.number_input("Ingrese valor de contraste: (0, +Inf)", value=2.3, placeholder="Ingrese un numero...")
+	image2 = cv2.addWeighted(img, contrast, np.zeros(img.shape, img.dtype), 0, brightness) 
+
+if option=="Paso Alto":
+	# Create the sharpening kernel 
+	kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]]) 
+	  
+	# Sharpen the image 
+	image2 = cv2.filter2D(img, -1, kernel) 
+
+if option=="Paso Bajo":
+	valor = st.number_input("Ingrese tamaño del filtro impar: (3, +Inf)", value=11, placeholder="Ingrese un numero...")
+	# Sharpen the image using the Laplacian operator 
+	image2 = cv2.medianBlur(img, valor) 
+	
+if option=="Color":
+	# Convert the image from BGR to HSV color space 
+	img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV) 
+	  
+	# Adjust the hue, saturation, and value of the image 
+	# Adjusts the hue by multiplying it by 0.7 
+	img[:, :, 0] = img[:, :, 0] * 0.7
+	# Adjusts the saturation by multiplying it by 1.5 
+	img[:, :, 1] = img[:, :, 1] * 1.5
+	# Adjusts the value by multiplying it by 0.5 
+	img[:, :, 2] = img[:, :, 2] * 0.5
+	  
+	# Convert the image back to BGR color space 
+	img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR) 
+	image2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+
+if option=="Ecualizacion":	
+	# Convert the image to grayscale 
+	gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+	  
+	# Equalize the histogram 
+	image2 = cv2.equalizeHist(gray_image) 
+	
+	
+col1, col2 = st.columns(2)
+if uploaded_file is not None:	
+
+	with col1:
+		st.header("Imagen original:")
+		st.image(img, caption='huella', width=200)
+
+	with col2:
+		st.header("Imagen mejorada: ")
+		st.image(image2, caption='huella', width=200)
+
---
+++ transf_geometrica1.py
@@ -0,0 +1,83 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+from ipywidgets import interact, interactive, fixed, interact_manual
+import ipywidgets as widgets
+import skimage as ski
+from skimage import exposure, io
+from skimage import transform
+import numpy as np
+import scipy.spatial.distance as dist
+from skimage.color import rgb2gray
+
+st.title('Correccion geometrica')
+
+uploaded_file = st.file_uploader("Seleccione una imagen...", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+img = io.imread(uploaded_file[0])
+#uploaded_file2 = st.file_uploader("Seleccione una imagen...", type=["jpg", "jpeg", "png"], key=2)
+#img_final = io.imread(uploaded_file[1])
+
+st.write(f"Dimensiones: ",img.shape)
+
+col1, col2 = st.columns(2)
+if uploaded_file is not None:	
+
+	with col1:
+		rotacion = st.number_input("Ingrese un valor de rotacion: (0.0-360.0)", value=0.0, placeholder="Ingrese un numero...")
+		escalado = st.number_input("Ingrese un valor de escalado: (-0.9,1.9)", value=0.0, placeholder="Ingrese un numero...")
+
+	with col2:
+		traslacionX = st.number_input("Ingrese un valor de traslacion en el eje X: (-Inf, +Inf)", value=0.0, placeholder="Ingrese un numero...")
+		traslacionY = st.number_input("Ingrese un valor de traslacion en el eje Y: (-Inf, +Inf)", value=0.0, placeholder="Ingrese un numero...")
+
+rotacion = np.deg2rad(rotacion)
+tform = transform.SimilarityTransform(scale=escalado, rotation= rotacion, translation=(traslacionX, traslacionY))
+st.write("Parametros de la correccion geometrica: ",tform.params)
+
+tf_img = ski.transform.warp(img, tform)
+#data = 255 * tf_img # Now scale by 255
+#img2 = data.astype(np.uint8)
+
+#st.write(f"Dimensiones: ",img2.shape)
+#st.write(f"Bit: ",img2.dtype)
+
+
+col3, col4 = st.columns(2)
+if uploaded_file is not None:	
+
+	with col3:
+		st.header("Imagen original:")
+		st.image(img, caption='gato original', width=200)
+
+	with col4:
+		st.header("Imagen corregida:")
+		st.image(tf_img, caption='gato_corregido (rotado, trasladado, escalado)', width=200)
+
+"""
+import cv2
+from io import BytesIO
+from io import BufferedReader
+
+image_rgb = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB) 
+
+ret, img_enco = cv2.imencode(".png", image_rgb)  #numpy.ndarray
+srt_enco = img_enco.tostring()  #bytes
+img_BytesIO = BytesIO(srt_enco) #_io.BytesIO
+img_BufferedReader = BufferedReader(img_BytesIO) #_io.BufferedReader
+
+btn = st.download_button(
+	label="Download",
+	data=img_BufferedReader,
+	file_name="cat_corrected.png",
+	mime="image/png"
+)
+
+im1_gray = rgb2gray(img)
+im2_gray = rgb2gray(img_final)
+
+dists = []
+for i in range(0, len(im1_gray)):
+	dists.append(dist.euclidean(im1_gray[i], im2_gray[i]))
+sum_dists = sum(dists)
+ave_dist = sum_dists/len(dists)
+st.write("Diferencia: ", ave_dist)
+"""
