43d799d2fe18b1233fc75c8eb76f358fc8bdc18f
LabSR-UT <142353737+LabSR-UT@users.noreply.github.com>
2025-09-07 21:44:37 -0500

    Update propiedades_imagen.py

--- propiedades_imagen.py
+++ propiedades_imagen.py
@@ -23,26 +23,34 @@ if uploaded_file is not None:
 col1, col2 = st.columns(2)
 
 if uploaded_file is not None:
-	with col1:
-		st.header("Imagen")
-		st.image(img, caption='imagen', width=200)
-	with col2:
-		st.header("Propiedades Imagen:")
-		st.write(f"Fecha de captura: ", exif['DateTime'][-20:-8])
-		st.write(f"Hora de captura: ", exif['DateTime'][-8:])
-		st.write(f"Ancho: ", exif['ImageWidth'])
-		st.write(f"Alto: ", exif['ImageLength'])
-		st.write(f"Bits por pixel: ", exif['BitsPerSample'])
+    with col1:
+        st.header("Imagen")
+        st.image(img, caption='imagen', width=200)
+    with col2:
+        try:    
+            st.header("Propiedades Imagen:")
+            st.write(f"Fecha de captura: ", exif['DateTime'][-20:-8])
+            st.write(f"Hora de captura: ", exif['DateTime'][-8:])
+            st.write(f"Ancho: ", exif['ImageWidth'])
+            st.write(f"Alto: ", exif['ImageLength'])
+            st.write(f"Bits por pixel: ", exif['BitsPerSample'])
+        except KeyError:
+            st.write("Información no disponible")
 		
 col3, col4 = st.columns(2)		
 if uploaded_file is not None:
-	with col3:
-		st.header("Propiedades Camara:")	
-		st.write(f"Marca: ", exif['Make'])
-		st.write(f"Modelo: ", exif['Model'])
-	with col4:
-		st.header("Configuracion Visualizacion:")
-		st.write(f"Contraste: ",exif['Contrast'])
-		st.write(f"Saturacion: ", exif['Saturation'])
-		st.write(f"Sharpness: ", exif['Sharpness'])		
-		
+    with col3:
+        try:
+            st.header("Propiedades Camara:")	
+            st.write(f"Marca: ", exif['Make'])
+            st.write(f"Modelo: ", exif['Model'])
+        except KeyError:
+            st.write("Información no disponible")
+    with col4:
+        try:
+            st.header("Configuracion Visualizacion:")
+            st.write(f"Contraste: ",exif['Contrast'])
+            st.write(f"Saturacion: ", exif['Saturation'])
+            st.write(f"Sharpness: ", exif['Sharpness'])		
+        except KeyError:
+            st.write("Información no disponible")
